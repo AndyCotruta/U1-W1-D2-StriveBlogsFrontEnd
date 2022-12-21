44249938df8b8526de7a21f3d7bef1f1b28b09d5
AndyCotruta <acotruta18@gmail.com>
2022-12-21 16:54:56 +0100

    fetch is working

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      let response = await fetch("http://localhost:3001/blogs");
+      try {
+        if (response.ok) {
+          let data = await response.json();
+          console.log(data);
+          setBlogs(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
   return (
     <Row>
-      {posts.map((post) => (
+      {blogs.map((blog) => (
         <Col
           md={4}
           style={{
             marginBottom: 50,
           }}
+          key={blog._id}
         >
-          <BlogItem key={post.title} {...post} />
+          <BlogItem key={blog.title} {...blog} />
         </Col>
       ))}
     </Row>
